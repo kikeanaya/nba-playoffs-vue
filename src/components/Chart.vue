6c8b941f5e26f94fa7d366109e1435f1ab4375a4
kikeanaya <luiseanayam@gmail.com>
2019-06-07 14:04:44 +0200

    chart works with games data

---
+++ src/components/Chart.vue
@@ -0,0 +1,72 @@
+<script>
+import { Line } from 'vue-chartjs'
+
+export default ({
+  extends: Line,
+
+  props: {
+    games: Array
+  },  
+  data () {
+    return {
+      gradient: null,
+      gradient2: null,
+      teamOneScores: [],
+      teamTwoScores: [],
+      teamOneName: '',
+      teamTwoName: '',
+      dates: []
+    }
+  },
+  mounted () {
+    this.gradient = this.$refs.canvas.getContext('2d').createLinearGradient(0, 0, 0, 450)
+    this.gradient2 = this.$refs.canvas.getContext('2d').createLinearGradient(0, 0, 0, 450)
+
+    this.gradient.addColorStop(0, 'rgba(255, 0,0, 0.5)')
+    this.gradient.addColorStop(0.5, 'rgba(255, 0, 0, 0.25)')
+    this.gradient.addColorStop(1, 'rgba(255, 0, 0, 0)')
+
+    this.gradient2.addColorStop(0, 'rgba(0, 231, 255, 0.9)')
+    this.gradient2.addColorStop(0.5, 'rgba(0, 231, 255, 0.25)')
+    this.gradient2.addColorStop(1, 'rgba(0, 231, 255, 0)')
+
+    this.teamOneName = this.games[0].home_team.abbreviation
+    this.teamTwoName = this.games[0].visitor_team.abbreviation
+
+    for(let i = 0; i<this.games.length; i++){
+      this.dates.push(this.games[i].date.slice(0,10))
+      if(this.games[i].home_team.abbreviation === this.teamOneName){
+        this.teamOneScores.push(this.games[i].home_team_score)
+        this.teamTwoScores.push(this.games[i].visitor_team_score)
+      } else{
+        this.teamOneScores.push(this.games[i].visitor_team_score)
+        this.teamTwoScores.push(this.games[i].home_team_score)
+      }
+    }  
+    
+    this.renderChart({
+      labels: this.dates, // partidos
+      datasets: [
+        {
+          label: this.teamOneName, // equipo
+          borderColor: '#FC2525',
+          pointBackgroundColor: 'white',
+          borderWidth: 1,
+          pointBorderColor: 'white',
+          backgroundColor: this.gradient,
+          data: this.teamOneScores // puntos
+        }, {
+          label: this.teamTwoName,
+          borderColor: '#05CBE1',
+          pointBackgroundColor: 'white',
+          pointBorderColor: 'white',
+          borderWidth: 1,
+          backgroundColor: this.gradient2,
+          data: this.teamTwoScores
+        }
+      ]
+    }, { responsive: true, maintainAspectRatio: false })
+  }
+})
+
+</script>
--- src/views/Series.vue
+++ src/views/Series.vue
@@ -1,64 +1,91 @@
 <template>
   <div>
+    <ul>
+      <li v-for="game in games">
+        {{game.home_team.abbreviation}}
+        {{game.home_team_score}}
+        {{game.visitor_team.abbreviation}}
+        {{game.visitor_team_score}}
+      </li>
+    </ul>
+    <Chart
+      v-if="games.length > 0"
+      :games="games"
+    />
   </div>
 </template>
 
 <script>
+import Chart from '@/components/Chart.vue'
 
 export default {
   data () {
     return {
-      games: []
+      games: [],
+      loaded: false
     }
   },
-  mounted () {
+  components: {
+    Chart
+  },
+   mounted () {
+     this.loaded = false
+
     switch (this.$route.params.id) {
       case 'lac-gsw': {
         fetch(`https://www.balldontlie.io/api/v1/games?team_ids[]=13&start_date=2019-04-13&end_date=2019-04-26`)
           .then(res => res.json())
           .then(myJson => { this.games = myJson.data })
+          .then(this.loaded = true)
         break
       }
       case 'uta-hou': {
         fetch(`https://www.balldontlie.io/api/v1/games?team_ids[]=29&start_date=2019-04-14&end_date=2019-04-24`)
           .then(res => res.json())
           .then(myJson => { this.games = myJson.data })
+          .then(this.loaded = true)
         break
       }
       case 'por-okc': {
         fetch(`https://www.balldontlie.io/api/v1/games?team_ids[]=25&start_date=2019-04-14&end_date=2019-04-24`)
           .then(res => res.json())
           .then(myJson => { this.games = myJson.data })
+          .then(this.loaded = true)
         break
       }
       case 'den-sas': {
         fetch(`https://www.balldontlie.io/api/v1/games?team_ids[]=27&start_date=2019-04-13&end_date=2019-04-27`)
           .then(res => res.json())
           .then(myJson => { this.games = myJson.data })
+          .then(this.loaded = true)
         break
       }
       case 'mil-det': {
         fetch(`https://www.balldontlie.io/api/v1/games?team_ids[]=17&start_date=2019-04-14&end_date=2019-04-22`)
           .then(res => res.json())
           .then(myJson => { this.games = myJson.data })
+          .then(this.loaded = true)
         break
       }
       case 'bos-ind': {
         fetch(`https://www.balldontlie.io/api/v1/games?team_ids[]=2&start_date=2019-04-14&end_date=2019-04-21`)
           .then(res => res.json())
           .then(myJson => { this.games = myJson.data })
+          .then(this.loaded = true)
         break
       }
       case 'phi-bkn': {
         fetch(`https://www.balldontlie.io/api/v1/games?team_ids[]=3&start_date=2019-04-13&end_date=2019-04-23`)
           .then(res => res.json())
           .then(myJson => { this.games = myJson.data })
+          .then(this.loaded = true)
         break
       }
       case 'tor-orl': {
         fetch(`https://www.balldontlie.io/api/v1/games?team_ids[]=28&start_date=2019-04-13&end_date=2019-04-23`)
           .then(res => res.json())
           .then(myJson => { this.games = myJson.data })
+          .then(this.loaded = true)
         break
       }
     }
