2f7f746a868bd2f6e79ddb0f65a40901b349b1ef
kikeanaya <luiseanayam@gmail.com>
2019-06-17 18:40:00 +0200

    changed css for main view

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link>
+      <router-link to="/" id="home-icon">Home</router-link>
     </div>
     <b-container>
       <router-view/>
